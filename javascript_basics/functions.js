2437ddcebacced2236c3d5b38ab085f5c6bc359a
anushyanu <93386625+anushyanu@users.noreply.github.com>
2022-12-30 08:05:42 +0000

    delete folder

--- javascript_basics/functions.js
+++
@@ -1,7 +0,0 @@
-function greeting(name){
-    // ` ` is used to add the strings with ${} for dynamic use
-    const word = `Hi ${name}, Nice to meet you`
-    console.log(word)
-}
-
-greeting("Yanu")
